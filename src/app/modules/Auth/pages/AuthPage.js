48e8ce9b1bdb57c48253c7f249a5cb23e5d7c493
qodirz <aq.abdulqodir@gmail.com>
2020-06-04 13:34:30 +0700

    fix login screen

--- src/app/modules/Auth/pages/AuthPage.js
+++ src/app/modules/Auth/pages/AuthPage.js
@@ -31,26 +31,26 @@ export function AuthPage() {
                 <Link to="/" className="flex-column-auto mt-5">
                   <img
                       alt="Logo"
-                      className="max-h-70px"
-                      src={toAbsoluteUrl("/media/logos/logo-letter-1.png")}
+                      className="max-h-70px max-w-70px"
+                      src={toAbsoluteUrl("/media/logos/flow-logo.svg")}
                   />
                 </Link>
                 {/* end:: Aside header */}
 
                 {/* start:: Aside content */}
                 <div className="flex-column-fluid d-flex flex-column justify-content-center">
-                  <h3 className="font-size-h1 mb-5 text-white">
-                    Welcome to FlowApp!
+                  <h3 className="font-size-h1 mb-5 text-white mt-1">
+                    Welcome to Flow
                   </h3>
-                  <p className="font-weight-lighter text-white opacity-80">
+                  {/* <p className="font-weight-lighter text-white opacity-80">
                     The ultimate Bootstrap & React 16 admin theme framework for next
                     generation web apps.
-                  </p>
+                  </p> */}
                 </div>
                 {/* end:: Aside content */}
 
                 {/* start:: Aside footer for desktop */}
-                <div className="d-none flex-column-auto d-lg-flex justify-content-between mt-10">
+                {/* <div className="d-none flex-column-auto d-lg-flex justify-content-between mt-10">
                   <div className="opacity-70 font-weight-bold	text-white">
                     &copy; 2020 Metronic
                   </div>
@@ -65,7 +65,7 @@ export function AuthPage() {
                       Contact
                     </Link>
                   </div>
-                </div>
+                </div> */}
                 {/* end:: Aside footer for desktop */}
               </div>
               {/*end: Aside Container*/}
@@ -95,7 +95,7 @@ export function AuthPage() {
               {/*end::Content body*/}
 
               {/* begin::Mobile footer */}
-              <div
+              {/* <div
                   className="d-flex d-lg-none flex-column-auto flex-column flex-sm-row justify-content-between align-items-center mt-5 p-5">
                 <div className="text-dark-50 font-weight-bold order-2 order-sm-1 my-2">
                   &copy; 2020 Metronic
@@ -117,7 +117,7 @@ export function AuthPage() {
                     Contact
                   </Link>
                 </div>
-              </div>
+              </div> */}
               {/* end::Mobile footer */}
             </div>
             {/*end::Content*/}
