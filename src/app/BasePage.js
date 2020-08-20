bb5aa25f3e334d1ff39d18efd9139f28893129ad
harwin <harwingalvis88@gmail.com>
2020-08-19 20:27:12 -0500

    add  /admin/contributors

--- src/app/BasePage.js
+++ src/app/BasePage.js
@@ -2,6 +2,7 @@ import React, {Suspense, lazy} from "react";
 import {Redirect, Switch, Route} from "react-router-dom";
 import {LayoutSplashScreen, ContentRoute} from "../_metronic/layout";
 import {BuilderPage} from "./pages/BuilderPage";
+import {AdminPagina} from "./pages/AdminPagina";
 import {MyPage} from "./pages/MyPage";
 import {DashboardPage} from "./pages/DashboardPage";
 import CountryPage from "./pages/CountryPage";
@@ -32,6 +33,7 @@ export default function BasePage() {
                 }
                 <ContentRoute path="/dashboard" component={DashboardPage}/>
                 <ContentRoute path="/builder" component={BuilderPage}/>
+                <ContentRoute path="/admin/contributors" component={AdminPagina}/>
                 <ContentRoute path="/my-page" component={MyPage}/>
                 <ContentRoute path="/countrypage" component={CountryPage}/>
                 <ContentRoute path="/currencypage" component={CurrencyPage}/>
