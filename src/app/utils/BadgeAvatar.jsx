fe76eeaa4f65cc8a880a132d1a513a5ebb8468b6
Zee <zeeshan111100@gmail.com>
2020-11-27 23:41:09 +0530

    F006 cleared user menu and removed view user tooltip

--- src/app/utils/BadgeAvatar.jsx
+++ src/app/utils/BadgeAvatar.jsx
@@ -14,57 +14,71 @@ const useStyles = makeStyles((theme) =>
   })
 );
 
-export default function BadgeAvatars({ src, onFileChange }) {
+export default function BadgeAvatars({ src, onFileChange, readOnly }) {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <Badge
-        overlap="rectangle"
-        anchorOrigin={{
-          vertical: "top",
-          horizontal: "right",
-        }}
-        badgeContent={
-          <label className="btn btn-xs btn-icon btn-circle btn-white btn-hover-text-primary btn-shadow">
-            <i className="fa fa-pen icon-sm text-muted"></i>
-            <input
-              type="file"
-              onChange={onFileChange}
-              hidden
-              accept=".png, .jpg, .jpeg"
-            />
-          </label>
-        }
-      >
+      {readOnly ? (
+        <Avatar
+          style={{
+            height: "100px",
+            width: "100px",
+            //   border: "1px solid #e4dcdc",
+            border: "5px solid #f3f3f3",
+          }}
+          variant="rounded"
+          alt="Travis Howard"
+          src={src}
+        />
+      ) : (
         <Badge
           overlap="rectangle"
           anchorOrigin={{
-            vertical: "bottom",
+            vertical: "top",
             horizontal: "right",
           }}
           badgeContent={
-            <span
-              onClick={onFileChange}
-              className="btn btn-xs btn-icon btn-circle btn-white btn-hover-text-primary btn-shadow"
-            >
-              <i className="ki ki-bold-close icon-xs text-muted"></i>
-            </span>
+            <label className="btn btn-xs btn-icon btn-circle btn-white btn-hover-text-primary btn-shadow">
+              <i className="fa fa-pen icon-sm text-muted"></i>
+              <input
+                type="file"
+                onChange={onFileChange}
+                hidden
+                accept=".png, .jpg, .jpeg"
+              />
+            </label>
           }
         >
-          <Avatar
-            style={{
-              height: "100px",
-              width: "100px",
-              //   border: "1px solid #e4dcdc",
-              border: "5px solid #f3f3f3",
+          <Badge
+            overlap="rectangle"
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "right",
             }}
-            variant="rounded"
-            alt="Travis Howard"
-            src={src}
-          />
+            badgeContent={
+              <span
+                onClick={onFileChange}
+                className="btn btn-xs btn-icon btn-circle btn-white btn-hover-text-primary btn-shadow"
+              >
+                <i className="ki ki-bold-close icon-xs text-muted"></i>
+              </span>
+            }
+          >
+            <Avatar
+              style={{
+                height: "100px",
+                width: "100px",
+                //   border: "1px solid #e4dcdc",
+                border: "5px solid #f3f3f3",
+              }}
+              variant="rounded"
+              alt="Travis Howard"
+              src={src}
+            />
+          </Badge>
         </Badge>
-      </Badge>
+      )}
     </div>
   );
 }
