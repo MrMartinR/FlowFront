b560c0bb91c95909b985e5c0e43d07b3ec4f3ad0
Zee <zeeshan111100@gmail.com>
2020-12-05 19:58:46 +0530

    updated user accounts

---
+++ src/app/pages/UserAccount/UserAccountCreateDialogHeader.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { shallowEqual, useSelector } from "react-redux";
+import { Modal, Badge } from "react-bootstrap";
+import { ModalProgressBar } from "../../../_metronic/_partials/controls";
+
+export const UserAccountCreateDialogHeader = ({ id }) => {
+  // Account Redux state
+  const { accountForEdit, actionsLoading } = useSelector(
+    (state) => ({
+      accountForEdit: state.accounts.accountForEdit,
+      actionsLoading: state.accounts.actionsLoading,
+    }),
+    shallowEqual
+  );
+
+  return (
+    <>
+      {actionsLoading && <ModalProgressBar />}
+      <Modal.Header closeButton>
+        <Modal.Title id="example-modal-sizes-title-lg">
+          {accountForEdit && id ? (
+            <label>
+              {" "}
+              Edit account{" "}
+              <Badge variant="secondary">{accountForEdit.name}</Badge>
+            </label>
+          ) : (
+            "Create Account"
+          )}
+        </Modal.Title>
+      </Modal.Header>
+    </>
+  );
+};
