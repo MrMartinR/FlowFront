b53ccbfda9c4833cb56d4a6ad49262e809dc8b23
Flow <hi@flowfin.tech>
2021-01-04 11:03:03 +0000

    Remove Unused Dependencies

--- src/_metronic/i18n/I18nProvider.js
+++ src/_metronic/i18n/I18nProvider.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { useLang } from './Metronici18n';
 import { IntlProvider } from 'react-intl';
 import '@formatjs/intl-relativetimeformat/polyfill';
-import '@formatjs/intl-relativetimeformat/dist/locale-data/en';
+import '@formatjs/intl-relativetimeformat/locale-data/en';
 
 import enMessages from './messages/en';
 
