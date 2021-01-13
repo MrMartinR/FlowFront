6f772a10b1640c01d9b05377b3f66b88f8b59b8c
James Okunlade <james.okunlade@gmail.com>
2021-01-13 19:56:29 +0100

    Authentication optionHeaders added to redux utils file to make it accessible to all redux files

--- src/redux/platforms/actions.tsx
+++ src/redux/platforms/actions.tsx
@@ -1,33 +1,10 @@
 import axios from 'axios';
-import store from '../store'
-import { API_URL } from '../utils'
+import { API_URL, optionsHeaders } from '../utils'
 
 export const REQUEST_PLATFORMSLIST = 'REQUEST_PLATFORMSLIST';
 export const RECEIVE_PLATFORMSLIST = 'RECEIVE_PLATFORMSLIST';
 export const ERROR_REQUESTING_PLATFORMSLIST = 'ERROR_REQUESTING_PLATFORMSLIST';
-
-const optionsHeaders = () => {
-    const {
-      auth: {
-        user, client, expiry, token,
-      },
-    } = store.getState()
   
-    const options = {
-      headers: {
-        'Referrer-Policy': 'strict-origin-when-cross-origin',
-        'Content-Type': 'application/json; charset=utf-8',
-        'access-token': token,
-        'token-type': 'Bearer',
-        client,
-        expiry,
-        uid: user.email,
-      },
-    }
-    return options
-}
-  
-
 function requestPlatformslist() {
     return {
         type: REQUEST_PLATFORMSLIST
--- src/redux/utils.tsx
+++ src/redux/utils.tsx
@@ -1,2 +1,28 @@
+import store from './store'
+
+
+// Backend API BASE URL
 export const API_URL = 'https://api.flowfin.tech'
 // export const API_URL = 'https://localhost:3001'
+
+// Authentication headers
+export const optionsHeaders = () => {
+    const {
+      auth: {
+        user, client, expiry, token,
+      },
+    } = store.getState()
+  
+    const options = {
+      headers: {
+        'Referrer-Policy': 'strict-origin-when-cross-origin',
+        'Content-Type': 'application/json; charset=utf-8',
+        'access-token': token,
+        'token-type': 'Bearer',
+        client,
+        expiry,
+        uid: user.email,
+      },
+    }
+    return options
+}
