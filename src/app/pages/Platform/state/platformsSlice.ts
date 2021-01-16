23f1ca91e8c7f707d17163d0065d7bfb0dad94bc
James Okunlade <james.okunlade@gmail.com>
2021-01-16 12:55:55 +0100

    platform redux moved into platform componenet folder

---
+++ src/app/pages/Platform/state/platformsSlice.ts
@@ -0,0 +1,72 @@
+import { createSlice } from '@reduxjs/toolkit'
+// import Util from '../../../utils'
+
+const initialPlatformsState = {
+  listLoading: true,
+  actionsLoading: false,
+  platformsTable: {
+    entities: null as any,
+    success: false
+  },
+  error: null as any,
+}
+
+export const callTypes = {
+  list: 'list',
+  action: 'action',
+}
+
+export const platformsSlice = createSlice({
+  name: 'platforms',
+  initialState: initialPlatformsState,
+  reducers: {
+    // when error occurs catch it 
+    catchError: (state, action) => {
+      state.error = `${action.type}: ${action.payload.error}`
+      if (action.payload.callType === callTypes.list) {
+        state.listLoading = false
+      } else {
+        state.actionsLoading = false
+      }
+    },
+    // set the state in which the process is in loading or setting the state
+    startCall: (state, action) => {
+      state.error = null
+      if (action.payload.callType === callTypes.list) {
+        state.listLoading = true
+      } else {
+        state.actionsLoading = true
+      }
+    },
+
+    // update the contact state on all fetch
+    contactsFetched: (state, action) => {
+      const { data } = action.payload
+      state.listLoading = false
+      state.error = null
+      state.contactsTable.entities = data.data
+      state.contactsTable.success = data.success
+    },
+
+    // update the contact state on fetch a single contact
+    contactFetched: (state, action) => {
+      const { data } = action.payload
+      state.listLoading = false
+      state.error = null
+      state.singleContact.entry = data.data
+      state.contactsTable.success = data.success
+    },
+
+    // on creation a new contact append it to existing contacts
+    newContactCreated: (state, action) => {
+      const { data } = action.payload
+      state.actionsLoading = false
+      state.error = null
+      state.contactsTable.entities.unshift(data.data)
+    },
+
+  
+  
+   
+  },
+})
