d081deed6fcbad14159ea3edf1abc80de0dd4d32
James Okunlade <james.okunlade@gmail.com>
2021-02-06 02:50:45 +0100

    :recycle: refactor code to display button when user is not invested in a loan

--- src/app/pages/UserLoan/UserLoanDetails.tsx
+++ src/app/pages/UserLoan/UserLoanDetails.tsx
@@ -7,126 +7,142 @@ const UserLoanDetails = (props: any) => {
   const { fetchUserLoanDetails, loan_id } = props
   const { userLoanDetails, loading } = props.userLoans
 
+  const [data, setData] = React.useState([] as any)
+
+  const processData = (obj: any) => {
+    let data = {} as any
+    for (const property in obj) {
+      data[`${property}`] = obj[property]
+    }
+    return data
+  }
+
   useEffect(() => {
     fetchUserLoanDetails(loan_id)
   }, [loan_id])
 
+  useEffect(() => {
+    setData(processData(userLoanDetails))
+  }, [userLoanDetails])
+
+  if (!loading && !userLoanDetails) {
+    return (
+      <div style={{ width: '100%', display: 'flex' }}>
+        <div style={{ width: '20%' }}></div>
+        <div style={{ width: '60%', textAlign: 'center' }}>
+          <Grid>
+            <CardContent>
+              <Typography variant="h5">You are not invested in this loan</Typography>
+              <button>Create an account</button>
+            </CardContent>
+          </Grid>
+        </div>
+        <div style={{ width: '20%' }}></div>
+      </div>
+    )
+  }
+
   return (
     <>
-      {!loading && !userLoanDetails ? (
-        <div style={{ width: '100%', display: 'flex' }}>
-          <div style={{ width: '20%' }}></div>
-          <div style={{ width: '60%', textAlign: 'center' }}>
-            <Grid>
-              <CardContent>
-                <Typography variant="h5">You are not invested in this loan</Typography>
-                <button>Create an account</button>
-              </CardContent>
+      <div style={{ width: '100%', display: 'flex' }}>
+        <div style={{ width: '20%' }}></div>
+        <div style={{ width: '60%', textAlign: 'center' }}>
+          <Grid>
+            <CardContent>
+              <Typography variant="h3">User Loan Details</Typography>
+            </CardContent>
+          </Grid>
+
+          <Grid container direction="row" justify="space-between" spacing={2}>
+            <Grid container direction="column" xs={6}>
+              <Grid>
+                <Card>
+                  <CardContent>
+                    <Typography>Market: {data.market}</Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
             </Grid>
-          </div>
-          <div style={{ width: '20%' }}></div>
+
+            <Grid container direction="column" xs={6}>
+              <Grid>
+                <Card>
+                  <CardContent>
+                    <Typography>Investment amount: {data.investment_amount}</Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            </Grid>
+          </Grid>
+
+          <Grid container direction="row" justify="space-between" spacing={2}>
+            <Grid container direction="column" xs={6}>
+              <Grid>
+                <Card>
+                  <CardContent>
+                    <Typography>Slice: {data.slice_name}</Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            </Grid>
+
+            <Grid container direction="column" xs={6}>
+              <Grid>
+                <Card>
+                  <CardContent>
+                    <Typography>Invest mode: {data.invest_mode}</Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            </Grid>
+          </Grid>
+
+          <Grid container direction="row" justify="space-between" spacing={2}>
+            <Grid container direction="column" xs={6}>
+              <Grid>
+                <Card>
+                  <CardContent>
+                    <Typography>Date in: {data.date_in}</Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            </Grid>
+
+            <Grid container direction="column" xs={6}>
+              <Grid>
+                <Card>
+                  <CardContent>
+                    <Typography>Date out: {data.date_out}</Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            </Grid>
+          </Grid>
+
+          <Grid container direction="row" justify="space-between" spacing={2}>
+            <Grid container direction="column" xs={6}>
+              <Grid>
+                <Card>
+                  <CardContent>
+                    <Typography>Position: {data.position}</Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            </Grid>
+
+            <Grid container direction="column" xs={6}>
+              <Grid>
+                <Card>
+                  <CardContent>
+                    <Typography>Xirr: {data.xirr}</Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            </Grid>
+          </Grid>
         </div>
-      ) : (
-        <div style={{ width: '100%', display: 'flex' }}>
-          <div style={{ width: '20%' }}></div>
-          <div style={{ width: '60%', textAlign: 'center' }}>
-            <Grid>
-              <CardContent>
-                <Typography variant="h3">User Loan Details</Typography>
-              </CardContent>
-            </Grid>
-
-            <Grid container direction="row" justify="space-between" spacing={2}>
-              <Grid container direction="column" xs={6}>
-                <Grid>
-                  <Card>
-                    <CardContent>
-                      <Typography>Market: {userLoanDetails.market}</Typography>
-                    </CardContent>
-                  </Card>
-                </Grid>
-              </Grid>
-
-              <Grid container direction="column" xs={6}>
-                <Grid>
-                  <Card>
-                    <CardContent>
-                      <Typography>Investment amount: {userLoanDetails.investment_amount}</Typography>
-                    </CardContent>
-                  </Card>
-                </Grid>
-              </Grid>
-            </Grid>
-
-            <Grid container direction="row" justify="space-between" spacing={2}>
-              <Grid container direction="column" xs={6}>
-                <Grid>
-                  <Card>
-                    <CardContent>
-                      <Typography>Slice: {userLoanDetails.slice_name}</Typography>
-                    </CardContent>
-                  </Card>
-                </Grid>
-              </Grid>
-
-              <Grid container direction="column" xs={6}>
-                <Grid>
-                  <Card>
-                    <CardContent>
-                      <Typography>Invest mode: {userLoanDetails.invest_mode}</Typography>
-                    </CardContent>
-                  </Card>
-                </Grid>
-              </Grid>
-            </Grid>
-
-            <Grid container direction="row" justify="space-between" spacing={2}>
-              <Grid container direction="column" xs={6}>
-                <Grid>
-                  <Card>
-                    <CardContent>
-                      <Typography>Date in: {userLoanDetails.date_in}</Typography>
-                    </CardContent>
-                  </Card>
-                </Grid>
-              </Grid>
-
-              <Grid container direction="column" xs={6}>
-                <Grid>
-                  <Card>
-                    <CardContent>
-                      <Typography>Date out: {userLoanDetails.date_out}</Typography>
-                    </CardContent>
-                  </Card>
-                </Grid>
-              </Grid>
-            </Grid>
-
-            <Grid container direction="row" justify="space-between" spacing={2}>
-              <Grid container direction="column" xs={6}>
-                <Grid>
-                  <Card>
-                    <CardContent>
-                      <Typography>Position: {userLoanDetails.position}</Typography>
-                    </CardContent>
-                  </Card>
-                </Grid>
-              </Grid>
-
-              <Grid container direction="column" xs={6}>
-                <Grid>
-                  <Card>
-                    <CardContent>
-                      <Typography>Xirr: {userLoanDetails.xirr}</Typography>
-                    </CardContent>
-                  </Card>
-                </Grid>
-              </Grid>
-            </Grid>
-          </div>
-          <div style={{ width: '20%' }}></div>
-        </div>
-      )}
+        <div style={{ width: '20%' }}></div>
+      </div>
     </>
   )
 }
