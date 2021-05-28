8cbc025fd361b5d483473c8576c648f5fa6be5bc
Flow <hi@flowfin.tech>
2021-05-28 21:56:28 +0100

    🎨  Lint Registration

--- src/app/modules/Auth/pages/Registration.tsx
+++ src/app/modules/Auth/pages/Registration.tsx
@@ -5,7 +5,18 @@ import * as authActions from '../state/authActions'
 import { useForm } from 'react-hook-form'
 import Logo from '../../../../common/media/flow-logo.svg'
 import { yupResolver } from '@hookform/resolvers/yup'
-import { TextField, Button, Grid, Typography, CardMedia, FormControl, Select, MenuItem, Modal, Card } from '@material-ui/core'
+import {
+  TextField,
+  Button,
+  Grid,
+  Typography,
+  CardMedia,
+  FormControl,
+  Select,
+  MenuItem,
+  Modal,
+  Card,
+} from '@material-ui/core'
 import { useDispatch } from 'react-redux'
 import { countriesList } from '../data/countries'
 import { terms, terms2, terms3, terms4, terms5, terms6 } from '../data/terms'
@@ -30,25 +41,16 @@ type RegisterType = {
  * @author Zeeshan A
  */
 export const Registration = () => {
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false)
   const RegistrationSchema = Yup.object().shape({
-    username: Yup.string()
-      .min(3, 'Minimum 3 characters')
-      .max(50, 'Maximum 50 characters')
-      .required('Required'),
+    username: Yup.string().min(3, 'Minimum 3 characters').max(50, 'Maximum 50 characters').required('Required'),
     email: Yup.string()
       .email('Wrong email format')
       .min(6, 'Minimum 6 characters')
       .max(50, 'Maximum 50 characters')
       .required('Required'),
-    password: Yup.string()
-      .min(3, 'Minimum 3 characters')
-      .max(50, 'Maximum 50 characters')
-      .required('Required'),
-    name: Yup.string()
-      .min(3, 'Minimum 3 characters')
-      .max(50, 'Maximum 50 characters')
-      .required('Required'),
+    password: Yup.string().min(3, 'Minimum 3 characters').max(50, 'Maximum 50 characters').required('Required'),
+    name: Yup.string().min(3, 'Minimum 3 characters').max(50, 'Maximum 50 characters').required('Required'),
     acceptTerms: Yup.boolean()
       .oneOf([true], 'You must accept the terms and conditions')
       .required('You must accept the terms and conditions'),
@@ -58,63 +60,90 @@ export const Registration = () => {
     resolver: yupResolver(RegistrationSchema),
     defaultValues: initialValues,
   })
-  const dispatch =  useDispatch();
-  const [country, setCountry] = useState('61c2888b-8b6a-4536-830f-3a14e86a9cd5');
+  const dispatch = useDispatch()
+  const [country, setCountry] = useState('61c2888b-8b6a-4536-830f-3a14e86a9cd5')
   const handleCountry = (e: any) => {
-    setCountry(e.target.value);
+    setCountry(e.target.value)
   }
 
   const onSubmit = ({ email, username, password, name }: RegisterType) => {
-    dispatch(authActions.registration(email, name, username, password, country));
+    dispatch(authActions.registration(email, name, username, password, country))
   }
 
   const handleOpen = () => {
-    setOpen(true);
-  };
+    setOpen(true)
+  }
 
   const handleClose = () => {
-    setOpen(false);
-  };
+    setOpen(false)
+  }
 
   const body = (
-    <Card style = {{ width : '80vh', maxHeight: '40vh', margin: 'auto', marginTop: '30vh', background: 'white', overflowY:'scroll', textAlign:'justify', padding:'20px' }}>
-      <Typography paragraph variant='h6'>Terms and conditions</Typography>
-      <Typography paragraph variant='body2'>{ terms }</Typography>
-      <Typography paragraph variant='body2'>{ terms2 }</Typography>
-      <Typography paragraph variant='body2'>{ terms3 }</Typography>
-      <Typography paragraph variant='body2'>{ terms4 }</Typography>
-      <Typography paragraph variant='body2'>{ terms5 }</Typography>
-      <Typography paragraph variant='body2'>{ terms6 }</Typography>
-      <Button variant = 'contained' fullWidth onClick = { handleClose }>Close</Button>
+    <Card
+      style={{
+        width: '80vh',
+        maxHeight: '40vh',
+        margin: 'auto',
+        marginTop: '30vh',
+        background: 'white',
+        overflowY: 'scroll',
+        textAlign: 'justify',
+        padding: '20px',
+      }}
+    >
+      <Typography paragraph variant="h6">
+        Terms and conditions
+      </Typography>
+      <Typography paragraph variant="body2">
+        {terms}
+      </Typography>
+      <Typography paragraph variant="body2">
+        {terms2}
+      </Typography>
+      <Typography paragraph variant="body2">
+        {terms3}
+      </Typography>
+      <Typography paragraph variant="body2">
+        {terms4}
+      </Typography>
+      <Typography paragraph variant="body2">
+        {terms5}
+      </Typography>
+      <Typography paragraph variant="body2">
+        {terms6}
+      </Typography>
+      <Button variant="contained" fullWidth onClick={handleClose}>
+        Close
+      </Button>
     </Card>
-  );
+  )
   return (
     <Grid container direction="column" justify="space-around" alignItems="center">
       {/* logo */}
       <Grid item xs="auto">
         <CardMedia src={Logo} component="img" />
         <Typography align="center" variant="h6">
-          Become a Flower!"
+          Flow Registration
         </Typography>
       </Grid>
       {/* form */}
       <Grid item xs="auto">
-      <form onSubmit={handleSubmit(onSubmit)} autoComplete="on">
+        <form onSubmit={handleSubmit(onSubmit)} autoComplete="on">
           <Grid container direction="column" justify="center" alignItems="center">
             {/* begin: Country */}
-            <FormControl variant = "outlined" margin = 'normal' style = {{ width: '100%' }}>
-              <Select labelId="Country" id="country" value = { country } onChange = { handleCountry }>
-                  {countriesList.map((country:any) => (
-                    <MenuItem 
-                      value= { country.id }
-                      key = { country.id }>{ country.name }</MenuItem>
-                  ))}  
+            <FormControl variant="outlined" margin="normal" style={{ width: '100%' }}>
+              <Select labelId="Country" id="country" value={country} onChange={handleCountry}>
+                {countriesList.map((country: any) => (
+                  <MenuItem value={country.id} key={country.id}>
+                    {country.name}
+                  </MenuItem>
+                ))}
               </Select>
             </FormControl>
             {/* end: Country */}
 
             {/* begin: Name */}
-            <FormControl variant="filled" style = {{ width: '100%' }}>
+            <FormControl variant="filled" style={{ width: '100%' }}>
               <TextField
                 id="outlined-uncontrolled"
                 label="Name"
@@ -128,9 +157,9 @@ export const Registration = () => {
               <span> {errors.name && errors.name.message}</span>
             </FormControl>
             {/* end: Name */}
-            
+
             {/* begin: Email */}
-            <FormControl variant="filled" style = {{ width: '100%' }}>
+            <FormControl variant="filled" style={{ width: '100%' }}>
               <TextField
                 id="outlined-uncontrolled"
                 label="Email"
@@ -146,7 +175,7 @@ export const Registration = () => {
             {/* end: Email */}
 
             {/* begin: Username */}
-            <FormControl variant="filled" style = {{ width: '100%' }}>
+            <FormControl variant="filled" style={{ width: '100%' }}>
               <TextField
                 id="outlined-uncontrolled"
                 label="Username"
@@ -162,13 +191,13 @@ export const Registration = () => {
             {/* end: Username */}
 
             {/* begin: Password */}
-            <FormControl variant="filled" style = {{ width: '100%' }}>
+            <FormControl variant="filled" style={{ width: '100%' }}>
               <TextField
                 id="outlined-uncontrolled"
                 label="Password"
                 margin="normal"
                 variant="outlined"
-                autoComplete="true"
+                autoComplete="false"
                 type="password"
                 name="password"
                 inputRef={register()}
@@ -180,30 +209,28 @@ export const Registration = () => {
             {/* begin: Terms and Conditions */}
             <FormControl variant="filled">
               <label htmlFor="acceptTerms" className="checkbox">
-              <Typography variant = 'body2'>
-                <input type="checkbox" name="acceptTerms" id="acceptTerms" ref={register()} />
-                I accept the 
-                  <span style = {{ color: 'blue', textDecoration : 'underline', cursor: 'pointer' }} onClick = { handleOpen }>
+                <Typography variant="body2">
+                  <input type="checkbox" name="acceptTerms" id="acceptTerms" ref={register()} />I accept the
+                  <span style={{ color: 'blue', textDecoration: 'underline', cursor: 'pointer' }} onClick={handleOpen}>
                     Terms & Conditions.
                   </span>
                 </Typography>
               </label>
-              <span> {errors.acceptTerms && errors.acceptTerms.message}</span><br/>
-              <Button type="submit" variant = 'contained' color = 'secondary'>
+              <span> {errors.acceptTerms && errors.acceptTerms.message}</span>
+              <br />
+              <Button type="submit" variant="contained" color="secondary">
                 <span>Sign Up</span>
               </Button>
-              <Typography variant = 'body2' align='center'>
-              Already registered? <Link rel="noreferrer" to="/auth/login">
+              <Typography variant="body2" align="center">
+                Already registered?{' '}
+                <Link rel="noreferrer" to="/auth/login">
                   Sign in
                 </Link>
               </Typography>
             </FormControl>
-            <Modal
-              open={open}
-              onClose={handleClose}
-            >
+            <Modal open={open} onClose={handleClose}>
               {body}
-          </Modal>
+            </Modal>
             {/* end: Terms and Conditions */}
           </Grid>
         </form>
